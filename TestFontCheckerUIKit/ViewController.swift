5ecc1b44c45de3af4d40da565a74301226dc374e
VyLeet <n.vysokinskyi@gmail.com>
2023-06-06 22:17:12 +0300

    Add font checking code

---
+++ TestFontCheckerUIKit/ViewController.swift
@@ -0,0 +1,112 @@
+//
+//  ViewController.swift
+//  TestFontCheckerUIKit
+//
+//  Created by Nazariy Vysokinskyi on 30.05.2023.
+//
+
+import UIKit
+import PDFKit
+import CoreGraphics
+import CoreText
+
+class ViewController: UIViewController, UIDocumentPickerDelegate {
+    // MARK: - Properties
+
+    static let shouldUseTextKit2 = true
+
+    let documentPicker = UIDocumentPickerViewController(documentTypes: ["com.adobe.pdf"], in: .import)
+    let pdfView = PDFView()
+    let textView: UITextView = {
+        if shouldUseTextKit2 {
+            return UITextView()
+        }
+
+        let textContainer = NSTextContainer()
+        let textStorage = NSTextStorage()
+        let layoutManager = NSLayoutManager()
+        textStorage.addLayoutManager(layoutManager)
+        layoutManager.addTextContainer(textContainer)
+
+        let view = UITextView(frame: CGRect.zero, textContainer: textContainer)
+        return view
+    }()
+
+    // MARK: - Setting up & Constraints
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .green
+
+        documentPicker.delegate = self
+
+        let chooseButton = UIButton(type: .system)
+        chooseButton.setTitle("Choose PDF", for: .normal)
+        chooseButton.addTarget(self, action: #selector(choosePDF), for: .touchUpInside)
+        chooseButton.translatesAutoresizingMaskIntoConstraints = false
+
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+
+        textView.translatesAutoresizingMaskIntoConstraints = false
+
+        view.addSubview(chooseButton)
+        view.addSubview(pdfView)
+        view.addSubview(textView)
+
+        NSLayoutConstraint.activate([
+            chooseButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            chooseButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            chooseButton.widthAnchor.constraint(equalToConstant: 200),
+            chooseButton.heightAnchor.constraint(equalToConstant: 50),
+
+            pdfView.topAnchor.constraint(equalTo: textView.bottomAnchor),
+            pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pdfView.bottomAnchor.constraint(equalTo: chooseButton.topAnchor),
+
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            textView.bottomAnchor.constraint(equalTo: self.view.centerYAnchor),
+            textView.topAnchor.constraint(equalTo: self.view.topAnchor)
+        ])
+    }
+
+    // MARK: - File picking
+
+    @objc func choosePDF() {
+        present(documentPicker, animated: true, completion: nil)
+    }
+
+    func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt urls: [URL]) {
+        if let url = urls.first {
+            let pdfDocument = PDFDocument(url: url)
+            pdfView.document = pdfDocument
+            renderPDFText(pdfDocument)
+        }
+    }
+
+    // MARK: - Text rendering
+
+    func renderPDFText(_ pdfDocument: PDFDocument?) {
+        guard let pdfDocument = pdfDocument else { return }
+
+        var pdfText = NSMutableAttributedString(string: "", attributes: nil)
+        var fontNames = Set<String>()
+
+        for pageIndex in 0..<pdfDocument.pageCount {
+            guard let pdfPage = pdfDocument.page(at: pageIndex) else { continue }
+
+            guard let attributedString = pdfPage.attributedString else { continue }
+            pdfText.append(attributedString)
+
+            attributedString.enumerateAttribute(.font, in: NSRange(location: 0, length: attributedString.length), options: []) { (value, range, _) in
+                if let font = value as? UIFont {
+                    fontNames.insert(font.fontName)
+                }
+            }
+        }
+
+        textView.attributedText = pdfText
+    }
+}
